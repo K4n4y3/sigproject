966a931f9014d6a683e03b01e2a21deec4a5bcd6
Sultan <sulta.hab@gmail.com>
2023-10-25 16:57:43 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { BsSteam } from 'react-icons/bs';
+import { AiFillGithub } from 'react-icons/ai';
 
 function App() {
   
@@ -14,9 +16,12 @@ function App() {
           <div className='header__logo'>Anime quotes</div>
 
           <nav className='nav'>
-            <a href='https://vk.com/maturboi' className='navlink'><div className='text'>VK</div></a>
-            <a href='https://steamcommunity.com/id/MATURBOII/' className='navlink'><div className='text'>Steam</div></a>
+            <a href='https://github.com/K4n4y3' className='navlink'><AiFillGithub size={20}/></a>
+            <a href='https://steamcommunity.com/id/MATURBOII/' className='navlink'><BsSteam size={20}/></a>
           </nav>
+        </div>
+        <div className='searchdiv'>
+        <input className='searchbar' type='text' placeholder='Enter a Quote'></input>
         </div>
       </div>
     </header>
